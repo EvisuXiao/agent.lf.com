3526522e44c5d64faac5f1a51d186e161e1b2cc4
Evisu Xiao <evisuxiao@gmail.com>
2019-03-22 12:28:11 +0800

    只推送dist

--- src/store/actions.js
+++
@@ -1,9 +0,0 @@
-import * as pageApi from '@/api/pageApi';
-
-// 获取页面配置信息
-export const loadPageConfig = ({ commit }) => {
-  pageApi.getPageConfig().then(res => {
-    console.log(res.result);
-    commit('SET_PAGE_INFO', res.result);
-  });
-};
